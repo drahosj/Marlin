60f24755bca8e34d57327f446cb3fd8a30dc0211
Jake <drahos.jake@gmail.com>
2014-06-15 01:54:10 -0500

    Changed Z axis steps per mm
    Set to calculated 2560 instead of empirical 2570 because Z ribbing.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -441,7 +441,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.024, 80.209, 2570, 870}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80.024, 80.209, 2560, 870}  // default steps per unit for Ultimaker
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 2, 16}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
